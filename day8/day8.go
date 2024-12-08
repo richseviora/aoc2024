f5c8a9144fe27488cf0bebfb59fc5d89a94eadd5
Rich Seviora <richard.seviora@gmail.com>
2024-12-08 14:35:02 -0800

    [DAY8] more work

--- day8/day8.go
+++ day8/day8.go
@@ -2,27 +2,95 @@ package day8
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"slices"
 	"strings"
 )
 
 type Grid struct {
-	Nodes []Node
+	Antennas    []Antenna
+	Height      int
+	Width       int
+	Frequencies map[string]bool
 }
 
-type Node struct {
-	Frequency string
-	Row       int
-	Column    int
+type Antenna struct {
+	Frequency  string
+	Coordinate Coordinate
+}
+
+type Coordinate struct {
+	Row int
+	Col int
 }
 
 func NewTableFromString(input string) Grid {
+	nodes := make([]Antenna, 0)
+	frequencies := make(map[string]bool)
 	rows := strings.Split(input, "\n")
 	for ri, row := range rows {
-		for ci, column := range strings.Split(row, "") {
-
+		for ci, value := range strings.Split(row, "") {
+			if value != "." {
+				if _, exists := frequencies[value]; !exists {
+					frequencies[value] = false
+				}
+				nodes = append(nodes, Antenna{
+					Frequency: value,
+					Coordinate: Coordinate{
+						Row: ri,
+						Col: ci,
+					},
+				})
+			}
 		}
 	}
+	return Grid{nodes, len(rows), len(rows[0]), make(map[string]bool)}
+}
+
+func (g *Grid) InBounds(c Coordinate) bool {
+	return c.Row >= 0 && c.Row < g.Height && c.Col >= 0 && c.Col < g.Width
+}
+
+func CalculateAntiNodes(a, b Coordinate, g *Grid) []Coordinate {
+	deltaY := float64(b.Row - a.Row)
+	deltaX := float64(b.Col - a.Col)
+	behind := Coordinate{
+		Row: int(float64(a.Row) - deltaY),
+		Col: int(float64(a.Col) - deltaX),
+	}
+	after := Coordinate{
+		Row: int(float64(b.Row) + deltaY),
+		Col: int(float64(b.Col) + deltaX),
+	}
+	coordinates := []Coordinate{behind, after}
+	deltaY3rd := deltaY / 3
+	deltaX3rd := deltaX / 3
+	if math.Mod(deltaY3rd, 1) == 0 && math.Mod(deltaX3rd, 1) == 0 {
+		mid1 := Coordinate{
+			Row: int(deltaY3rd) + a.Row,
+			Col: int(deltaX3rd) + a.Col,
+		}
+		mid2 := Coordinate{
+			Row: int(deltaY3rd) + (a.Row * 2),
+			Col: int(deltaX3rd) + (a.Col * 2),
+		}
+		coordinates = append(coordinates, mid1, mid2)
+	}
+	validValues := slices.DeleteFunc(coordinates, func(c Coordinate) bool {
+		return !g.InBounds(c)
+	})
+	return validValues
+}
+
+func (g *Grid) GetAntennasForFrequency(value string) []Antenna {
+	antennas := make([]Antenna, 0)
+	for _, a := range g.Antennas {
+		if a.Frequency == value {
+			antennas = append(antennas, a)
+		}
+	}
+	return antennas
 }
 
 const filename = "input.txt"
@@ -44,5 +112,7 @@ func ReadInput(fname string) string {
 }
 
 func main() {
+	contents := ReadInput(filename)
+	table := NewTableFromString(contents)
 
 }
