91104b0b212216a0a80f81914b3cadc0f4d72e25
Rich Seviora <richard.seviora@gmail.com>
2024-12-11 09:48:27 -0800

    [DAY11] Start

---
+++ day11/day11.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var testFileName = "test.txt"
+var actualFileName = "input.txt"
+
+func ReadInput(fname string) string {
+	file, err := os.Open(fname)
+	defer file.Close()
+	if err != nil {
+		fmt.Printf("error received: %e", err)
+		panic(err)
+	}
+
+	content, err := os.ReadFile(fname)
+	if err != nil {
+		fmt.Printf("error received: %v", err)
+		panic(err)
+	}
+	return string(content)
+}
+
+func HandleFile(fname string) {
+	fileContent := ReadInput(fname)
+	for _, pt2 := range []bool{false} {
+		fmt.Println(fname, pt2, len(fileContent))
+	}
+
+}
+
+func main() {
+	HandleFile(testFileName)
+	HandleFile(actualFileName)
+}
